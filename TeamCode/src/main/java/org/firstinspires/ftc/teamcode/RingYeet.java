b239b9fd5dae19441f6b94318ad0742dd6cce2c4
YEtestacc <478967@bsd48.org>
2025-06-01 16:42:57 -0700

    blocks 2.0

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RingYeet.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RingYeet.java
@@ -1,53 +1,55 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
 
 @TeleOp //(name = "ringyeet (Blocks to Java)")
-public class RingYeet extends LinearOpMode {
+public class RingYeet extends OpMode {
 
-    private DcMotor RingYeetMotor;
-    private DcMotor MainIntake;
-    private DcMotor CounterspinMotor;
-    private DcMotor biggearslowMotor;
+    private DcMotor ringYeetMotor;
+    private DcMotor mainIntake;
+    private DcMotor counterspinMotor;
+    private DcMotor bigGearSlowMotor;
 
     @Override
-    public void runOpMode() {
-        RingYeetMotor = hardwareMap.get(DcMotor.class, "Ring Yeet Motor");
-        MainIntake = hardwareMap.get(DcMotor.class, "Main Intake");
-        CounterspinMotor = hardwareMap.get(DcMotor.class, "Counter spin Motor");
-        biggearslowMotor = hardwareMap.get(DcMotor.class, "big gear slow Motor");
+    public void init() {
+        ringYeetMotor = hardwareMap.get(DcMotor.class, "Ring Yeet Motor");
+        mainIntake = hardwareMap.get(DcMotor.class, "Main Intake");
+        counterspinMotor = hardwareMap.get(DcMotor.class, "Counter spin Motor");
+        bigGearSlowMotor = hardwareMap.get(DcMotor.class, "big gear slow Motor");
 
-        waitForStart();
-        if (opModeIsActive()) {
-            RingYeetMotor.setDirection(DcMotor.Direction.FORWARD);
-            while (opModeIsActive()) {
-                if (gamepad1.a) {
-                    RingYeetMotor.setPower(1);
-                } else {
-                    RingYeetMotor.setPower(0);
-                }
-                if (gamepad1.b) {
-                    MainIntake.setPower(-1);
-                    CounterspinMotor.setPower(-1);
-                } else {
-                    MainIntake.setPower(0);
-                    CounterspinMotor.setPower(0);
-                }
-                if (gamepad1.right_bumper) {
-                    biggearslowMotor.setPower(0.5);
-                } else {
-                    biggearslowMotor.setPower(0);
-                }
-                if (gamepad1.left_bumper) {
-                    biggearslowMotor.setPower(-0.5);
-                } else {
-                    biggearslowMotor.setPower(0);
-                }
-                // Put loop blocks here.
-                telemetry.update();
-            }
+        ringYeetMotor.setDirection(DcMotor.Direction.FORWARD);
+    }
+
+    @Override
+    public void loop() {
+        if (gamepad1.a) {
+            ringYeetMotor.setPower(1);
+        } else {
+            ringYeetMotor.setPower(0);
+        }
+
+        if (gamepad1.b) {
+            mainIntake.setPower(-1);
+            counterspinMotor.setPower(-1);
+        } else {
+            mainIntake.setPower(0);
+            counterspinMotor.setPower(0);
+        }
+
+        if (gamepad1.right_bumper) {
+            bigGearSlowMotor.setPower(0.5);
+        } else {
+            bigGearSlowMotor.setPower(0);
+        }
+
+        if (gamepad1.left_bumper) {
+            bigGearSlowMotor.setPower(-0.5);
+        } else {
+            bigGearSlowMotor.setPower(0);
         }
     }
 }
